e495057c4759920866883fbb2b29ba8acce5ecdb
newhobbye <robsonhobbye@gmail.com>
2022-01-06 06:55:22 -0300

    Projeto Iniciado!

---
+++ src/main/java/br/com/mercado/mercadosb/MercadosbApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercado.mercadosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mercado/mercadosb/MercadosbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercado.mercadosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
