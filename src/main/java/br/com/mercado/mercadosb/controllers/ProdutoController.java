db0b427d61ab9b8a5bd81badabcbbb99c697e131
newhobbye <robsonhobbye@gmail.com>
2022-01-10 20:07:03 -0300

    Alterando metodos PUT e POST, Inserindo metodo DEL.

--- src/main/java/br/com/mercado/mercadosb/controllers/ProdutoController.java
+++ src/main/java/br/com/mercado/mercadosb/controllers/ProdutoController.java
@@ -1,11 +1,14 @@
 package br.com.mercado.mercadosb.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,33 +18,34 @@ import br.com.mercado.mercadosb.model.entities.repositories.ProdutoRepository;
 @RestController
 @RequestMapping("/api/produto")
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@GetMapping
-	public Iterable<Produto> getProduto(){
+	public Iterable<Produto> getProduto() {
 		return produtoRepository.findAll();
-		
+
 	}
+
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome,
-			@RequestParam Double precoVarejo,@RequestParam Double precoAtacado
-			,@RequestParam Integer qtd,@RequestParam String categoria) {
-		
-		Produto produto = new Produto(nome, precoVarejo, precoAtacado, qtd, categoria);
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
-		
+
 	}
+
 	@PutMapping
-	public Produto alterarProduto(@RequestParam String nome, 
-			@RequestParam Double precoAtacado,@RequestParam Double precoVarejo,
-			@RequestParam Integer qtd,@RequestParam String categoria) {
-		Produto produto = new Produto(nome, precoVarejo, precoAtacado, qtd, categoria);
+	public @ResponseBody Produto alterarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
-		
+
 	}
+	@DeleteMapping("/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
+	}
+		
+	
 
 }
